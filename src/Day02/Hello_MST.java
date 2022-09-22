5329277ceb9bb963ffc81ec8528f45be236e5c08
mst <mstfkvk5600@gmail.com>
2022-09-22 11:37:47 +0300

    creating first java code, "System.out.println();"

---
+++ src/Day02/Hello_MST.java
@@ -0,0 +1,13 @@
+package Day02;
+
+public class Hello_MST {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Musty");
+        System.out.println("Wooden Spoon");
+
+    }
+
+}
+
