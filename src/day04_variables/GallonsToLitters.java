7c7d3bc6af3eceeb076a19f9d94c5fc8f034aab1
mst <mstfkvk5600@gmail.com>
2022-09-23 20:31:15 +0300

    Escape sequence ex and variables

---
+++ src/day04_variables/GallonsToLitters.java
@@ -0,0 +1,14 @@
+package day04_variables;
+
+public class GallonsToLitters {
+    public static void main(String[] args) {
+        int gallon= 1000;
+        double lt= gallon* 3.79;
+        System.out.println("lt = " + lt);
+    }
+}
+/*
+* 5. Create a class named GallonsToLitters, Write a program that can convert gallon (int) to litter (double)
+					Hints: 1 gallon = 3.79 litters
+							1000
+							10000*/
